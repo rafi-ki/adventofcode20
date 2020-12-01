a4d797a38b130b547f8770c3bb3bab0b891c6601
Rafael Teyral <r.teyral@tricentis.com>
2020-12-01 23:06:21 +0100

    day one

--- adventofcode/Resolver.fs
+++ adventofcode/Resolver.fs
@@ -3,11 +3,23 @@
 module DayOne =
     open CommonTypes
 
-    let calculateFuel (mass: int) =
-        mass ./. 3 |> floor |> int |> fun x -> x - 2
+    let solve1 lines =
+        let mutable answer = 0;
+        for line in lines do
+            let matchingLine = lines |> Array.except [|line|] |> Seq.tryFind (fun x -> x + line = 2020)
+            if matchingLine.IsSome then answer <- matchingLine.Value * line
+        answer |> string
+
+    let solve2 lines =
+        let mutable answer = 0;
+        for line in lines do
+            for second in lines |> Array.except [|line|] do
+                let matchingLine = lines |> Array.except [|line;second|] |> Seq.tryFind (fun x -> x + line + second = 2020)
+                if matchingLine.IsSome then answer <- matchingLine.Value * line * second
+        answer |> string
 
     let solve puzzle =
-        puzzle.Lines
-        |> Array.map (fun x -> x |> int |> calculateFuel)
-        |> Array.sum
-        |> string
+        let lines = puzzle.Lines |> Array.map int
+        if puzzle.Part = 1
+        then solve1 lines
+        else solve2 lines
--- test/DayOne.fs
+++ test/DayOne.fs
@@ -1,6 +1,5 @@
 module Tests
 
-open System
 open Xunit
 open CommonTypes
 open adventofcode20
@@ -8,32 +7,23 @@ open Resolver
 
 
 module DayOne =
-    [<Fact>]
-    let ``solve empty returns 0``() =
-        let puzzle = {
-            Day = 1
-            Part = 1
-            Lines = Array.empty
-        }
-        let result = DayOne.solve puzzle
-        Assert.Equal("0", result)
 
     [<Fact>]
-    let ``solve 1969 returns 654``() =
+    let ``solve given example part one``() =
         let puzzle = {
             Day = 1
             Part = 1
-            Lines = [|"1969"|]
+            Lines = [|"1721";"979";"366";"299";"675";"1456"|]
         }
         let result = DayOne.solve puzzle
-        Assert.Equal("654", result)
+        Assert.Equal("514579", result)
 
     [<Fact>]
-    let ``solve 100756 returns 33583``() =
+    let ``solve given example part two``() =
         let puzzle = {
             Day = 1
-            Part = 1
-            Lines = [|"100756"|]
+            Part = 2
+            Lines = [|"1721";"979";"366";"299";"675";"1456"|]
         }
         let result = DayOne.solve puzzle
-        Assert.Equal("33583", result)
+        Assert.Equal("241861950", result)
